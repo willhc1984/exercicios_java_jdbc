062a7ccca41c3e63eb471e34a5a1238e7a1eb267
will henrique <willhc1984@gmail.com>
2022-06-14 14:33:15 -0300

    Delete

---
+++ src/application/Delete.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import jdbc.DB;
+import jdbc.DbIntegrityException;
+
+public class Delete {
+
+	public static void main(String[] args) {
+		
+		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement(""
+					+ " DELETE FROM department "
+					+ " WHERE id = ?");
+			
+			st.setInt(1, 4);
+			int rowsAffected = st.executeUpdate();
+			System.out.println("Rows affected: " + rowsAffected);
+			
+		} catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		}finally {
+			DB.closeStatment(st);
+			DB.closeConnection();
+		}
+
+	}
+
+}
