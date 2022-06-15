c8df2291b9e7b070af9f719d2a748cd6eadbd0fa
will henrique <willhc1984@gmail.com>
2022-06-15 11:32:19 -0300

    Seller - findById Refactor

--- src/application/Test.java
+++ src/application/Test.java
@@ -12,6 +12,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		
+		System.out.println("***** Test 1 - Seller findById *******");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(7);
 		System.out.println(seller);
